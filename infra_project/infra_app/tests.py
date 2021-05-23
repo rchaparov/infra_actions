b39bf474f730873ab9007dc32f15b7cc48d4cd6d
Ramis Chaparov <r.chaparov@gmail.com>
2021-05-23 15:09:46 +0400

    add test with flake8

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -11,14 +11,12 @@ class StaticPagesURLTests(TestCase):
         """Проверка доступности страниц."""
         response = self.guest_client.get('/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
-
         response = self.guest_client.get('/second_page/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_page_shows_correct_context(self):
         """Проверка контекста страниц."""
         response = self.guest_client.get('/')
-        self.assertContains(response, 'У меня получилось!')
-        
+        self.assertContains(response, 'У меня получилось!')       
         response = self.guest_client.get('/second_page/')
         self.assertContains(response, 'А это вторая страница!')
--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -1,11 +1,8 @@
+from django.urls import path
 from . import views
 
-from django.urls import path
-
-
 app_name = 'infra_app'
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('second/', views.second_page, name='second_page'),
-    ]
+]
